07f64fbe9d06a1cd81b2556b3be391fc8b2f5141
Virginie Lemaire <virginielemaire@free.fr>
2026-06-13 20:56:56 +0200

    fix(contest status): fix export import of getContestStatus

--- src/features/contests/index.ts
+++ src/features/contests/index.ts
@@ -1,7 +1,8 @@
 export { DAY_MS } from "./constants";
 export type { Contest, CreateContestInput, ContestContextValue, ContestStatus } from "./types";
 export { ContestProvider, DEV_SCENARIO_CHANGE_EVENT } from "./ContestContext";
-export { STATUS_LABEL, STATUS_COLOR } from "./contestStatus"
+export { STATUS_LABEL, STATUS_COLOR, getContestStatus } from "./contestStatus"
 export { useContests } from "./useContests";
 export { canEditContest, canDeleteContest } from "./permissions";
 export type { UpdateContestInput } from "./types";
+
